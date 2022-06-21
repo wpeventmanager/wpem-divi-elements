497964778a2d7bd17aa6c1338fe6f15dd23fc484
Work Of Development <vijayrathod245@gmail.com>
2022-06-21 16:01:25 +0530

    #48

--- includes/modules/PastEventListing/PastEventListing.php
+++ includes/modules/PastEventListing/PastEventListing.php
@@ -122,7 +122,11 @@ class WPEM_Past_Event_Listing extends ET_Builder_Module {
 		return $fields;
 	}
 	
-	public function render( $attrs, $content, $render_slug ) {
+	function past_event_listing( $args = array(), $conditional_tags = array(), $current_page = array() ) {
+		foreach ( $args as $arg => $value ) {
+			$this->props[ $arg ] = $value;
+		}
+
 		$post_id            = isset( $current_page['id'] ) ? (int) $current_page['id'] : 0;
 		$posts_number       = $this->props['per_page'];
 		$order       		= $this->props['order'];
@@ -132,33 +136,47 @@ class WPEM_Past_Event_Listing extends ET_Builder_Module {
 		$categories       		= $this->props['include_categories'];
 		$event_types       		= $this->props['include_event_types'];
 		$show_pagination       		= $this->props['show_pagination'];
-		
+
+
 		$shortcode = sprintf(
-		
-			'[events per_page="%1$s" event_types="%9$s" categories="%10$s" show_pagination="%12$s"]',
-			esc_attr( $layout ),
+			'[events  per_page="%1$s" order="%2$s" orderby="%3$s"]',
 			esc_attr( $posts_number ),
 			esc_attr( $order ),
 			esc_attr( $orderby ),
-			esc_attr( $keywords ),
 			esc_attr( $location ),
-			esc_attr( $cancelled ),
-			esc_attr( $featured ),
-			esc_attr( $event_types ),
+			esc_attr( $keywords ),
 			esc_attr( $categories ),
-			esc_attr( $show_filters ),
+			esc_attr( $event_types ),
 			esc_attr( $show_pagination ),
+		
 		);
-		
+		wp_enqueue_script( 'chosen');
+		wp_enqueue_script( 'wp-event-manager-content-event-listing');
+		wp_enqueue_script( 'wp-event-manager-ajax-filters');
+
+		do_action( 'et_pb_event_before_print_event_listing' );
+
 		$output_events = do_shortcode( $shortcode );
-		
+
+		do_action( 'et_pb_event_after_print_event_listing' );
+
+		return $output_events;
+	}
+	public function render( $attrs, $content, $render_slug ) {
+		$posts_number            = $this->props['per_page'];
+		$pagination              = $this->props['show_pagination'];
+		$order       		= $this->props['order'];
+		$orderby       		= $this->props['orderby'];
+		$location       		= $this->props['location'];
+		$keywords       		= $this->props['keywords'];
+		$categories       		= $this->props['include_categories'];
+		$event_types       		= $this->props['include_event_types'];
 		$output = sprintf(
 			'<div>
 				%1$s
 			</div>',
-			$output_events
+			$this->past_event_listing( array(), array(), array( 'id' => $this->get_the_ID() ) )
 		);
-		
 		return $output;
 	}
 }
